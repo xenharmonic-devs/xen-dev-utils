84d3c502ecae3cbbd8535cfa4267c81bc06df4f8
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-07-28 13:03:34 +0300

    Implement an algorithm for simplifying spellings within a temperament

--- src/__tests__/monzo.spec.ts
+++ src/__tests__/monzo.spec.ts
@@ -3,6 +3,7 @@ import {Fraction} from '../fraction';
 import {
   monzoToFraction,
   primeLimit,
+  spell,
   toMonzo,
   toMonzoAndResidual,
 } from '../monzo';
@@ -120,5 +121,38 @@ describe('Prime limit calculator', () => {
   it('can handle large inputs', () => {
     const limit = primeLimit(new Fraction(4294967296, 4006077075));
     expect(limit).toBe(13);
+  });
+});
+
+describe('Respeller', () => {
+  it('knows that 81/64 should be spelled 5/4 in meantone', () => {
+    const spelling = spell('81/64', ['81/80']);
+    expect(spelling.minBenedetti.equals('5/4')).toBeTruthy();
+  });
+
+  it('knows that the major third is 9/7 in pajara', () => {
+    const third = new Fraction('3/2').div('7/5').pow(4);
+    const spelling = spell(third, ['50/49', '64/63']);
+    expect(spelling.minBenedetti.equals('9/7')).toBeTruthy();
+    expect(spelling.noTwosMinNumerator.equals('32/25')).toBeTruthy();
+    expect(spelling.noTwosMinDenominator.equals('81/64')).toBeTruthy();
+  });
+
+  it('knows that the minor third is 7/6 is in pajara', () => {
+    const third = new Fraction('4/3').pow(3).div('10/7').div('10/7');
+    const spelling = spell(third, ['50/49', '64/63']);
+    expect(spelling.minBenedetti.equals('7/6')).toBeTruthy();
+  });
+
+  it('knows that the minor third is 6/5 in srutal', () => {
+    const third = new Fraction('3/2').div('45/32').pow(3);
+    const spelling = spell(third, ['2048/2025', '4375/4374']);
+    expect(spelling.minBenedetti.equals('6/5')).toBeTruthy();
+  });
+
+  it('knows that the major third is 5/4 is in srutal', () => {
+    const third = new Fraction('4/3').pow(2).div('45/32');
+    const spelling = spell(third, ['2048/2025', '4375/4374']);
+    expect(spelling.minBenedetti.equals('5/4')).toBeTruthy();
   });
 });
